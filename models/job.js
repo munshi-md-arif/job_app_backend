e137af6df6c1e755486429af845fc4bb2d6b8de3
MunshiMdArif <munshi.a@kestoneglobal.com>
2025-05-15 23:56:33 +0530

    updated

--- models/job.js
+++ models/job.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const jobSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true,
+        required: false,
+
     },
     location: {
         type: String,
