045b23a46c8a43cb33d4b2b7d47b364c7d4db9c6
MunshiMdArif <munshi.a@kestoneglobal.com>
2025-05-16 12:34:01 +0530

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const port = 3400;
+const MONGO_URL = "mongodb+srv://arifmunshi278:lGC0mA2aZCi9LPdC@cluster0.cnf5jqe.mongodb.net/personalProject";
 const jobouter = require("./routes/job");
 const bodyParser = require("body-parser");
 
@@ -13,7 +14,7 @@ mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
-    console.log(process.env.MONGO_URL, "Connected to MongoDB");
+    console.log(MONGO_URL, "Connected to MongoDB");
     console.log("✅ Connected to MongoDB");
 }).catch((err) => {
     console.error("❌ MongoDB connection failed:", err);
